7c74b4805ae594974e40cc69429e4c495399de7a
kraiglarson2 <kraigalarson2@gmail.com>
2024-01-22 14:15:27 -0500

    Initial Commit

---
+++ Currency/CurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyApp.swift
+//  Currency
+//
+//  Created by Krajg Larson on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
